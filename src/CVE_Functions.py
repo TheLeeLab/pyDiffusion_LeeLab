97a814b6933da03f37d8c1532a3de3b7e6af3d49
jbeckwith <jsbeckwith@gmail.com>
2024-04-25 10:51:20 +0100

    Deleted un-needed files

--- src/CVE_Functions.py
+++ src/CVE_Functions.py
@@ -13,8 +13,8 @@ class CVE():
         self = self
         return
     
-    def Eq14(self, x, t, R=1./6):
-        """ CVE_Eq16 function
+    def Eq14(self, x, t, R=1./6, min_points=10):
+        """ CVE_Eq14 function
         takes positions and uses equation 14 from
         Vestergaard, C. L.; Blainey, P. C.; Flyvbjerg, H
         Phys. Rev. E 2014, 89 (2), 022726. 
@@ -25,12 +25,23 @@ class CVE():
         Args:
             x (np.1darray): 1D positions
             t (np.1darray): time
-            R (float): R parameter (see Equation 5 of paper)
+            R (float): motion blur parameter (see Equation 5 of paper)
+            min_points (float): minimum track length
         
         Returns:
             D (float): diffusion coefficient estimate
             varD (float): variance of D 
         """
+        try:
+            if x.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+
+        
         diffX = np.diff(x)
         mult = np.mean(np.multiply(diffX[:-1], diffX[1:]))
         deltaX_sqr = np.mean(np.square(diffX))
@@ -44,7 +55,7 @@ class CVE():
         varD = np.multiply(np.square(D), (((6 + 4*epsilon + 2*np.square(epsilon))/N) + ((4*np.square(1+epsilon))/np.square(N))))
         return D, sigma, varD
     
-    def Eq16(self, x, sigma, t, R=1./6):
+    def Eq16(self, x, sigma, t, R=1./6, min_points=10):
         """ CVE_Eq16 function
         takes positions and uses equation 16 from
         Vestergaard, C. L.; Blainey, P. C.; Flyvbjerg, H
@@ -57,20 +68,34 @@ class CVE():
             sigma (np.1darray): precision of estimations of x
             t (np.1darray): time
             R (float): R parameter (see Equation 5 of paper)
-        
+            min_points (float): minimum track length
+
         Returns:
             D (float): diffusion coefficient estimate
             varD (float): variance of D 
         """
+        try:
+            if x.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+
         diffX = np.diff(x)
         sigma_squared = np.square(np.mean((sigma)))
         deltaX_sqr = np.mean(np.square(diffX))
         deltat = np.mean(np.unique(np.diff(t)))
-        D = np.divide(np.subtract(deltaX_sqr, np.multiply(2., sigma_squared)), np.multiply(np.multiply(2, np.subtract(1., np.multiply(2., R))), deltat))
+        D = np.divide(np.subtract(deltaX_sqr, np.multiply(2., sigma_squared)), 
+                      np.multiply(np.multiply(2, np.subtract(1., np.multiply(2., R))), deltat))
         varsigma = np.var(np.square(sigma))
-        epsilon = np.subtract(np.divide(np.square(sigma), np.multiply(D, deltat)), np.multiply(2, R))
+        epsilon = np.subtract(np.divide(np.square(sigma), 
+                    np.multiply(D, deltat)), np.multiply(2, R))
         N = len(x)
-        varD = np.add(np.divide(np.multiply(np.square(D), (2 + 4*epsilon + 3*np.square(epsilon))), N*np.square(1 - 2*R)), np.divide(varsigma, (np.square(deltat)*np.square(1 - 2*R))))
+        varD = np.add(np.divide(np.multiply(np.square(D), 
+                (2 + 4*epsilon + 3*np.square(epsilon))), N*np.square(1 - 2*R)),
+                     np.divide(varsigma, (np.square(deltat)*np.square(1 - 2*R))))
         
         return D, varD
     
