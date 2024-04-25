e37dda62d286edb5e8018289253d8d788754e132
jbeckwith <jsbeckwith@gmail.com>
2024-04-25 10:43:09 +0100

    Updated, Fixed OLSF

--- src/Diffusion_MLE.py
+++ src/Diffusion_MLE.py
@@ -17,7 +17,7 @@ class D_MLE():
         self = self
         return
     
-    def DSigma_MLE_BootStrap(self, coordinates, dT, R=1./6, n_d=1, maxiter=100000, maxfun=100000, n_samples=100):
+    def DSigma_MLE_BootStrap(self, coordinates, dT, R=1./6, n_d=1, maxiter=100000, maxfun=100000, n_samples=1000, min_points=10):
         """
         Compute diffusion coefficient error estimate, and estimate of the
         error on the dynamic localisation error, using bootstrapping.
@@ -30,25 +30,44 @@ class D_MLE():
                         dimension should be same shape as this number
             maxiter (int): maximum number of optimisation iterations to make
             maxfun (int): maximum number of function evaluations to make
-            n_samples (int): number of boostrapped samples. default 100.
+            n_samples (int): number of boostrapped samples. default 1000.
+            min_points (int): minimum number of points for a diffusion estimate.
+                            Default is 10.
     
         Returns:
             D_err (float): estimate of D value error
             sigma_err (float): estimate of dynamic localisation std error
         """
+        if n_d > 1:
+            try:
+                if coordinates.shape[1] != n_d:
+                    raise Exception('Dimension of coordinates and n_d are inconsistent.')
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                return np.NAN, np.NAN
+            
+        try:
+            if coordinates.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+
         D_err = np.zeros(n_samples)
         sigma_err = np.zeros(n_samples)
         
         samples = np.diff(coordinates, axis=0).ravel()
         for i in np.arange(n_samples):
-            diff_coordinates = np.random.choice(samples, size=(coordinates.shape[0], n_d))
+            diff_coordinates = np.random.choice(samples, size=(coordinates.shape[0]-1, n_d))
             var_diff_coordinates = np.var(diff_coordinates, axis=0)
             D_i = np.mean(var_diff_coordinates) / (2 * dT * 2)        
             sigma2_i = np.mean(var_diff_coordinates) / 2
     
             # make displacements array
-            dX = np.subtract(np.diff(coordinates, axis=0),
-                             np.tile(np.mean(np.diff(coordinates, axis=0), axis=0),
+            dX = np.subtract(diff_coordinates,
+                             np.tile(np.mean(diff_coordinates, axis=0),
                                      (len(coordinates) - 1, 1)))
             
             sine_transform = np.square(dst(dX, 2, axis=0)/2.)
@@ -59,7 +78,7 @@ class D_MLE():
         return np.std(D_err), np.std(sigma_err)
 
     
-    def DSigma_MLE(self, coordinates, dT, R=1./6, n_d=1, maxiter=100000, maxfun=100000):
+    def DSigma_MLE(self, coordinates, dT, R=1./6, n_d=1, maxiter=100000, maxfun=100000, min_points=10):
         """
         Compute diffusion coefficient estimate, and estimate of the
         dynamic localisation error, using the MLE approach.
@@ -72,12 +91,31 @@ class D_MLE():
                         dimension should be same shape as this number
             maxiter (int): maximum number of optimisation iterations to make
             maxfun (int): maximum number of function evaluations to make
-    
+            min_points (int): minimum number of points for a diffusion estimate.
+                            Default is 10.
+
         Returns:
             D (float): estimate of D value.
             sigma (float): estimate of dynamic localisation std.
         """
         # get initial guess of D and Sigma2
+        if n_d > 1:
+            try:
+                if coordinates.shape[1] != n_d:
+                    raise Exception('Dimension of coordinates and n_d are inconsistent.')
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                return np.NAN, np.NAN
+            
+        try:
+            if coordinates.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+
         diff_coordinates = np.diff(coordinates, axis=0)
         var_diff_coordinates = np.var(diff_coordinates, axis=0)
         D_i = np.mean(var_diff_coordinates) / (2 * dT * 2)        
--- src/MSDFunctions.py
+++ src/MSDFunctions.py
@@ -12,6 +12,150 @@ class MSD():
     def __init__(self):
         self = self
         return
+    
+    def DSigma2_OLSF_BootStrap(self, coordinates, dT, R=1./6, n_d=1, maxiter=100, min_points=10, n_samples=1000):
+        """
+        Compute diffusion coefficient error estimate, and estimate of the
+        dynamic localisation variance, using bootstrapping of the OLSF MSD approach.
+    
+        Args:
+            coordinates (numpy.ndarray): Input trajectory.
+            dT (float): Time interval.
+            R (float): Motion blur coefficient.
+            n_d (int): number of dimensions. If above 1, coordinates second
+                        dimension should be same shape as this number
+            maxiter (int): Maximum number of iterations. Defaults to 100.
+            min_points (int): minimum number of points for a diffusion estimate.
+                            Default is 10.
+            n_samples (int): number of bootstrapped samples. Default 1000.
+
+        Returns:
+            D_error (float): Diffusion coefficient error estimate.
+            var_error (float): var error estimate.
+        """
+        if n_d > 1:
+            try:
+                if coordinates.shape[1] != n_d:
+                    raise Exception('Dimension of coordinates and n_d are inconsistent.')
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                return np.NAN, np.NAN
+            
+        try:
+            if coordinates.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+        
+        D_err = np.zeros(n_samples)
+        var_err = np.zeros(n_samples)
+        
+        samples = np.diff(coordinates, axis=0).ravel()
+        r0 = np.zeros([n_d]) # initial position is 0s
+        for i in np.arange(n_samples):
+            displacements = np.random.choice(samples, size=(coordinates.shape[0]-1, n_d))
+            new_coordinates = np.vstack([r0, np.cumsum(displacements, axis=0)])
+            D_err[i], var_err[i] = self.DSigma2_OLSF(new_coordinates, dT, R=R, n_d=n_d)
+        return np.std(D_err), np.std(var_err)
+    
+    def DSigma2_OLSF(self, coordinates, dT, R=1./6, n_d=1, maxiter=100, min_points=10):
+        """
+        Compute diffusion coefficient estimate, and estimate of the
+        dynamic localisation error, using the OLSF MSD approach.
+    
+        Args:
+            coordinates (numpy.ndarray): Input trajectory.
+            dT (float): Time interval.
+            R (float): Motion blur coefficient.
+            n_d (int): number of dimensions. If above 1, coordinates second
+                        dimension should be same shape as this number
+            maxiter (int): Maximum number of iterations. Defaults to 100.
+            min_points (int): minimum number of points for a diffusion estimate.
+                            Default is 10.
+
+        Returns:
+            D (float): Diffusion coefficient estimate.
+            var (float): var estimate.
+        """
+        if n_d > 1:
+            try:
+                if coordinates.shape[1] != n_d:
+                    raise Exception('Dimension of coordinates and n_d are inconsistent.')
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                return np.NAN, np.NAN
+            
+        try:
+            if coordinates.shape[0] < min_points:
+                raise Exception("""Not enough points to calculate a reliable estimate of diffusion coefficient.
+                                Please see section V of Michalet and Berglund, Optimal Diffusion Coefficient Estimation
+                                in Single-Particle Tracking. Phys. Rev. E 2012, 85 (6), 061916. https://doi.org/10.1103/PhysRevE.85.061916.""")
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+            return np.NAN, np.NAN
+
+
+        NXM = len(coordinates.ravel())
+        pa = np.array([int(np.floor(NXM / 10))])
+        pb = np.array([int(np.floor(NXM / 10))])
+        donea = 0
+        doneb = 0
+        iter = 1
+        rho = []
+        while iter <= maxiter and (not donea or not doneb):
+            if iter == maxiter:
+                donea = True
+                doneb = True
+                D = np.nan
+                var = np.nan
+                print('OLSF did not converge in {} iterations'.format(maxiter))
+            else:
+                if np.max(np.hstack([pa, pb])) > len(rho):
+                    rho = self.msd_fft(coordinates)
+                rho = rho[:np.max(np.hstack([pa, pb]))]
+                if not donea:
+                    A = np.vstack((np.ones(pa[-1]), np.arange(1, pa[-1] + 1))).T
+                    B = rho[:A.shape[0]]
+                    if B.shape[0] != A.shape[0]:
+                        donea = True
+                        doneb = True
+                        D = np.nan
+                        var = np.nan
+                        print('OLSF did not converge in {} iterations'.format(maxiter))
+                        return D, var
+                    X = np.linalg.lstsq(A, B, rcond=None)[0]
+                    aa, ba = X
+                    xa = 0 if aa < 0 else np.inf if ba < 0 else aa / ba
+                    newpa = self.PMin_XM(xa, NXM)
+                    if np.any(np.isin(newpa, pa)):
+                        Da = ba / (2 * dT * n_d)
+                        var = (aa + 4 * Da * R * dT) / (2 * n_d)
+                        donea = True
+                    pa = np.hstack([pa, newpa])
+                if not doneb:
+                    A = np.vstack((np.ones(pb[-1]), np.arange(1, pb[-1] + 1))).T
+                    B = rho[:int(pb[-1])]
+                    if B.shape[0] != A.shape[0]:
+                        donea = True
+                        doneb = True
+                        D = np.nan
+                        var = np.nan
+                        print('OLSF did not converge in {} iterations'.format(maxiter))
+                        return D, var
+                    X = np.linalg.lstsq(A, B, rcond=None)[0]
+                    ab, bb = X
+                    xb = 0 if ab < 0 else np.inf if bb < 0 else ab / bb
+                    newpb = self.PMin_XM(xb, NXM)
+                    if np.any(np.isin(newpb, pb)):
+                        D = bb / (2 * dT * n_d)
+                        doneb = True
+                    pb = np.hstack([pb, newpb])
+                iter += 1
+
+        return D, var
 
     def autocorrFFT(self, x):
         """
@@ -53,4 +197,37 @@ class MSD():
         for m in range(N):
             Q=Q-D[m-1]-D[N-m]
             S1[m]=Q/(N-m)
-        return S1-2*S2
+        S = S1-2*S2
+        return S[1:]
+    
+    def PMin_XM(self, x, N):
+        """
+        Calculate optimal fit point from XM formulas.
+        These formulas use XM conventions for N. These differ from AB.
+        
+        Args:
+            x (float): Input value.
+            N (int): Number of trajectory points.
+        
+        Returns:
+            pa (int): Optimal fit point for parameter 'a'.
+            pb (int): Optimal fit point for parameter 'b'.
+        """
+        fa = 2 + 1.6 * (x ** 0.51)
+        La = 3 + ((4.5 * (N ** 0.4) - 8.5) ** 1.2)
+        
+        fb = 2 + 1.35 * x ** 0.6
+        Lb = 0.8 + (0.564 * N)
+        
+        if np.isinf(x):
+            pa = int(np.floor(La))
+            pb = int(np.floor(Lb))
+        else:
+            pa = int(np.floor(fa * La / (fa ** 3 + La ** 3) ** 0.33))
+            pb = min(int(np.floor(Lb)), int(np.floor(fb * Lb / (fb ** 3 + Lb ** 3) ** 0.33)))
+        
+        # Make sure nothing is zero
+        pa = max(2, pa)
+        pb = max(2, pb)
+        
+        return pa, pb
